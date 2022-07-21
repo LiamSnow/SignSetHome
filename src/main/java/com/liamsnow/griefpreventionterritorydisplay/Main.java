80b4d708e7adfdce3fcaff19a48c7ce214a195cc
Liam Snow <liamsnow03@gmail.com>
2022-07-21 15:07:58 -0400

    Spigot Setup

---
+++ src/main/java/com/liamsnow/griefpreventionterritorydisplay/Main.java
@@ -0,0 +1,18 @@
+package com.liamsnow.griefpreventionterritorydisplay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		System.out.println("FUCK");
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+		System.out.println("FUCK2");
+	}
+}

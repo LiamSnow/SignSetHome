83e6c372c3b9ce6fd53b18089b40cb8ec26413c7
Liam Snow <liamsnow03@gmail.com>
2022-07-23 23:07:28 -0400

    GriefPrevention Event Handling!

--- src/main/java/me/liamsnow/signsethome/eventhandlers/GriefProtectionEventHandler.java
+++ src/main/java/me/liamsnow/signsethome/eventhandlers/GriefProtectionEventHandler.java
@@ -7,6 +7,7 @@ import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.events.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -51,7 +52,7 @@ public class GriefProtectionEventHandler implements Listener {
 		Collection<Claim> changedClaims = event.getClaims();
 
 		for (Claim changedClaim : changedClaims) {
-			verifyPlayerHomesInClaim(changedClaim.getID(), changedClaim.getOwnerID()));
+			verifyPlayerHomesInClaim(changedClaim.getID(), changedClaim.getOwnerID());
 		}
 	}
 
@@ -64,27 +65,34 @@ public class GriefProtectionEventHandler implements Listener {
 	}
 
 	private void removePlayerHome(UUID playerUUID, UUID responsiblePlayerUUID) {
-		//TODO
-
-//		SignSetHome.instance.getServer().broadcastMessage("REMOVING THE HOME OF " + playerUUID);
-
 		//Remove Home Sign
 		if (DataFileHandler.hasValidHomeLocation(playerUUID)) {
-			DataFileHandler.getHomeLocation(player).getBlock().setType(Constants.REPLACE_OLD_HOME_MATERIAL);
-			player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "Removed old home.");
+			DataFileHandler.getHomeLocation(playerUUID).getBlock().setType(Constants.REPLACE_OLD_HOME_MATERIAL);
 		}
 
 		//Removed Saved Home Location
 		DataFileHandler.removeHomeLocation(playerUUID);
 
-		//Notify the Player that their home was removed
+		//Notify the Player they removed their own home
+		Player player = Bukkit.getPlayer(playerUUID);
+		if (responsiblePlayerUUID != null && playerUUID.compareTo(responsiblePlayerUUID) == 0) {
+			player.sendMessage(ChatColor.RED + "You're decision making led to your home being destroyed.");
+		}
 
+		else {
+			//Notify the Player that their home was removed
+			if (player != null) {
+				player.sendMessage(ChatColor.RED + "As a result of poor territorial leadership, you're set home was removed.");
+			}
 
-		//Notify the Devil of the homes and lives they ruined :(
-		if (responsiblePlayerUUID != null) {
-			Player responsiblePlayer = Bukkit.getPlayer(responsiblePlayerUUID);
-			if (responsiblePlayer != null) {
-				responsiblePlayer.sendMessage(ChatColor.RED + "Removing the home of " + playerUUID);
+			//Notify the Devil of the homes and lives they ruined :(
+			if (responsiblePlayerUUID != null) {
+				Player responsiblePlayer = Bukkit.getPlayer(responsiblePlayerUUID);
+				OfflinePlayer effectedPlayer = Bukkit.getOfflinePlayer(playerUUID);
+				String effectedPlayerName = effectedPlayer == null ? "Unknown" : effectedPlayer.getName();
+				if (responsiblePlayer != null) {
+					responsiblePlayer.sendMessage(ChatColor.RED + "You destroyed the peaceful and loving home of " + effectedPlayerName);
+				}
 			}
 		}
 	}
